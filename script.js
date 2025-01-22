31bb4524f9b8de87bed2a80a9b640d08ef824ae5
João Pedro de Oliveira <jao07oliveiragames@gmail.com>
2025-01-22 11:54:58 -0300

    att

--- script.js
+++ script.js
@@ -8,19 +8,22 @@ const inputName = document.getElementById("cardholder-name"); // Input Name
 const inputNumberCard = document.getElementById("cardholder-number"); // Input Number
 const inputExpDate = document.getElementById("cardholder-expdate"); // Input Expdate
 const inputMMYY = document.getElementById("cardholder-mmyy"); // Input MMYY
-const inputCVC = document.getElementById("cardholder-cvc"); // Input CVC
+const inputCVC = document.getElementById("cardholder-cvc"); // Input CVC7
+const $productContainer = document.getElementById("productContainer")
 const nameField = document.getElementById("nameField");
-const numberCardField = document.getElementById("numberCardField")
-const cvcField = document.getElementById("cvc-field")
-const expdateField = document.getElementById("expdate-field")
-const yyfield = document.getElementById("yy-field")
-const checkName = document.querySelector("#nameField .bi-check2-circle")
-const errorName = document.querySelector("#nameField .bi-x-circle")
-const checkNumberCard = document.querySelector("#numberCardField .bi-check2-circle")
-const errorNumberCard = document.querySelector("#numberCardField .bi-x-circle")
-const checkcvc = document.querySelector("#cvc-field .bi-check2-circle")
-const errorcvc = document.querySelector("#cvc-field .bi-x-circle")
-const formComplete = document.getElementById("form-complete")
+const numberCardField = document.getElementById("numberCardField");
+const cvcField = document.getElementById("cvc-field");
+const expdateField = document.getElementById("expdate-field");
+const yyfield = document.getElementById("yy-field");
+const checkName = document.querySelector("#nameField .bi-check2-circle");
+const errorName = document.querySelector("#nameField .bi-x-circle");
+const checkNumberCard = document.querySelector(
+  "#numberCardField .bi-check2-circle"
+);
+const errorNumberCard = document.querySelector("#numberCardField .bi-x-circle");
+const checkcvc = document.querySelector("#cvc-field .bi-check2-circle");
+const errorcvc = document.querySelector("#cvc-field .bi-x-circle");
+const formComplete = document.getElementById("form-complete");
 
 numberCard.innerText = "0000 0000 0000 0000";
 nameCard.innerText = "Jane Appleseed";
@@ -29,157 +32,177 @@ dateCardYY.innerText = "00";
 cvcCard.innerText = "000";
 
 inputName.addEventListener("input", () => {
-    const valueName = inputName.value
-    const regexName = /^[a-zA-ZÀ-ÿ\s]+$/ // Regex para permitir apenas letras e valida a string inteira
-    const isValidName = regexName.test(valueName)
-    if (isValidName === true && valueName.length >= 3){
-        errorName.classList.add("d-none")
-        checkName.classList.remove("d-none")
-        inputName.classList.add("form-control-check")
-        inputName.classList.remove("form-control-error")
-        nameField.classList.remove("form-message-error-name")
-        nameField.classList.remove("form-message-error-null")
+  const valueName = inputName.value;
+  const regexName = /^[a-zA-ZÀ-ÿ\s]+$/; // Regex para permitir apenas letras e valida a string inteira
+  const isValidName = regexName.test(valueName);
+  if (isValidName === true && valueName.length >= 3) {
+    errorName.classList.add("d-none");
+    checkName.classList.remove("d-none");
+    inputName.classList.add("form-control-check");
+    inputName.classList.remove("form-control-error");
+    nameField.classList.remove("form-message-error-name");
+    nameField.classList.remove("form-message-error-null");
+  } else {
+    checkName.classList.add("d-none");
+    errorName.classList.remove("d-none");
+    inputName.classList.remove("form-control-check");
+    inputName.classList.add("form-control-error");
+    nameField.classList.add("form-message-error-name");
+    nameField.classList.remove("form-message-error-null");
+    if (valueName === "") {
+      nameField.classList.add("form-message-error-null");
+      inputName.classList.remove("form-control-check");
     }
-    else{
-        checkName.classList.add("d-none")
-        errorName.classList.remove("d-none")
-        inputName.classList.remove("form-control-check")
-        inputName.classList.add("form-control-error")
-        nameField.classList.add("form-message-error-name")
-        nameField.classList.remove("form-message-error-null")
-        if(valueName === ""){
-            nameField.classList.add("form-message-error-null")
-            inputName.classList.remove("form-control-check")
-        }
-    }
-    nameCard.innerHTML = ""
-    nameCard.innerText += `${valueName}`
-})
+  }
+  nameCard.innerHTML = "";
+  nameCard.innerText += `${valueName}`;
+});
 
 inputNumberCard.addEventListener("input", () => {
-    const valueNumberCard = inputNumberCard.value.replace(/\D/g, '')// Regex para remover letras
-    const regexNumberCard = /(\d{4})(?=\d)/g // Regex para que captura um grupo de 4 digitos
-    const formatNumberCard = valueNumberCard.replace(regexNumberCard, "$1 ").slice(0,19) // formata o valor do input para add espaços
-    inputNumberCard.value = formatNumberCard // Faz que o valor do input seja formatado tbm
-    if(valueNumberCard.length > 16){
-        inputNumberCard.classList.remove("form-control-check")
-        inputNumberCard.classList.add("form-control-error")
-        numberCardField.classList.add("form-message-error-limit")
-        checkNumberCard.classList.add("d-none")
-        errorNumberCard.classList.remove("d-none")
-    }
-    else if(valueNumberCard.length < 16){ 
-        inputNumberCard.classList.remove("form-control-check")
-        numberCardField.classList.remove("form-message-error-limit")
-        inputNumberCard.classList.add("form-control-error")
-        numberCardField.classList.add("form-message-error-null")
-        checkNumberCard.classList.add("d-none")
-        errorNumberCard.classList.remove("d-none")
-    }
-    else{
-        inputNumberCard.classList.add("form-control-check")
-        inputNumberCard.classList.remove("form-control-error")
-        numberCardField.classList.remove("form-message-error-null")
-        checkNumberCard.classList.remove("d-none")
-        errorNumberCard.classList.add("d-none")
-        numberCardField.classList.remove("form-message-error-limit")
-    }
-    numberCard.innerHTML = ""
-    numberCard.innerHTML += `${formatNumberCard}`
-})
-
+  const valueNumberCard = inputNumberCard.value.replace(/\D/g, ""); // Regex para remover letras
+  const regexNumberCard = /(\d{4})(?=\d)/g; // Regex para que captura um grupo de 4 digitos
+  const formatNumberCard = valueNumberCard
+    .replace(regexNumberCard, "$1 ")
+    .slice(0, 19); // formata o valor do input para add espaços
+  inputNumberCard.value = formatNumberCard; // Faz que o valor do input seja formatado tbm
+  if (valueNumberCard.length > 16) {
+    inputNumberCard.classList.remove("form-control-check");
+    inputNumberCard.classList.add("form-control-error");
+    numberCardField.classList.add("form-message-error-limit");
+    checkNumberCard.classList.add("d-none");
+    errorNumberCard.classList.remove("d-none");
+  } else if (valueNumberCard.length < 16) {
+    inputNumberCard.classList.remove("form-control-check");
+    numberCardField.classList.remove("form-message-error-limit");
+    inputNumberCard.classList.add("form-control-error");
+    numberCardField.classList.add("form-message-error-null");
+    checkNumberCard.classList.add("d-none");
+    errorNumberCard.classList.remove("d-none");
+  } else {
+    inputNumberCard.classList.add("form-control-check");
+    inputNumberCard.classList.remove("form-control-error");
+    numberCardField.classList.remove("form-message-error-null");
+    checkNumberCard.classList.remove("d-none");
+    errorNumberCard.classList.add("d-none");
+    numberCardField.classList.remove("form-message-error-limit");
+  }
+  numberCard.innerHTML = "";
+  numberCard.innerHTML += `${formatNumberCard}`;
+});
 
 let valueExpDate;
 inputExpDate.addEventListener("input", () => {
-    valueExpDate = inputExpDate.value
-    if(valueExpDate !== undefined && valueExpDate !== "MM"){
-        inputExpDate.classList.add("form-control-check")
-        expdateField.classList.remove("form-message-error-select")
-    }
-    else{
-        inputExpDate.classList.remove("form-control-check")
-    }
-    dateCardMM.innerHTML = ""
-    if(valueExpDate < 10){
-        dateCardMM.innerHTML += "0" + valueExpDate
-    }
-    else{
-        dateCardMM.innerHTML += valueExpDate
-    }
-})
+  valueExpDate = inputExpDate.value;
+  if (valueExpDate !== undefined && valueExpDate !== "MM") {
+    inputExpDate.classList.add("form-control-check");
+    expdateField.classList.remove("form-message-error-select");
+  } else {
+    inputExpDate.classList.remove("form-control-check");
+  }
+  dateCardMM.innerHTML = "";
+  if (valueExpDate < 10) {
+    dateCardMM.innerHTML += "0" + valueExpDate;
+  } else {
+    dateCardMM.innerHTML += valueExpDate;
+  }
+});
 
 let valueYY;
 inputMMYY.addEventListener("input", () => {
-    valueYY = inputMMYY.value
-    if(valueYY !== undefined && valueYY !== "YY"){
-        inputMMYY.classList.add("form-control-check")
-        yyfield.classList.remove("form-message-error-select")
-    }
-    else{
-        inputMMYY.classList.remove("form-control-check")
-    }
-    dateCardYY.innerHTML = ""
-    if(valueYY < 10){
-        dateCardYY.innerHTML += "0" + valueYY
-    }
-    else{
-        dateCardYY.innerHTML += valueYY
-    }
-})
+  valueYY = inputMMYY.value;
+  if (valueYY !== undefined && valueYY !== "YY") {
+    inputMMYY.classList.add("form-control-check");
+    yyfield.classList.remove("form-message-error-select");
+  } else {
+    inputMMYY.classList.remove("form-control-check");
+  }
+  dateCardYY.innerHTML = "";
+  if (valueYY < 10) {
+    dateCardYY.innerHTML += "0" + valueYY;
+  } else {
+    dateCardYY.innerHTML += valueYY;
+  }
+});
 
-inputCVC.addEventListener("input", () =>{
-    const valueCVC = inputCVC.value
-    const regexCVC = /^[0-9]+$/g
-    const isValidCVC = regexCVC.test(valueCVC)
-    if(isValidCVC === true && valueCVC.length === 3){
-        inputCVC.classList.add("form-control-check")
-        inputCVC.classList.remove("form-control-error")
-        checkcvc.classList.remove("d-none")
-        errorcvc.classList.add("d-none")
-        cvcField.classList.remove("form-message-error-cvc")
-    }
-    else{
-        inputCVC.classList.remove("form-control-check")
-        inputCVC.classList.add("form-control-error")
-        checkcvc.classList.add("d-none")
-        errorcvc.classList.remove("d-none")
-        cvcField.classList.add("form-message-error-cvc")   
-    }
-    cvcCard.innerHTML = ""
-    cvcCard.innerHTML += valueCVC
-})
+inputCVC.addEventListener("input", () => {
+  const valueCVC = inputCVC.value;
+  const regexCVC = /^[0-9]+$/g;
+  const isValidCVC = regexCVC.test(valueCVC);
+  if (isValidCVC === true && valueCVC.length === 3) {
+    inputCVC.classList.add("form-control-check");
+    inputCVC.classList.remove("form-control-error");
+    checkcvc.classList.remove("d-none");
+    errorcvc.classList.add("d-none");
+    cvcField.classList.remove("form-message-error-cvc");
+  } else {
+    inputCVC.classList.remove("form-control-check");
+    inputCVC.classList.add("form-control-error");
+    checkcvc.classList.add("d-none");
+    errorcvc.classList.remove("d-none");
+    cvcField.classList.add("form-message-error-cvc");
+  }
+  cvcCard.innerHTML = "";
+  cvcCard.innerHTML += valueCVC;
+});
 
 form.addEventListener("submit", (event) => {
-    event.preventDefault()
-    let hasError = false
-    if(valueExpDate === undefined || valueExpDate === "MM"){
-        hasError = true
-        expdateField.classList.add("form-message-error-select")
-        inputExpDate.classList.add("form-control-error")
+//   event.preventDefault();
+  let hasError = false;
+  if (valueExpDate === undefined || valueExpDate === "MM") {
+    hasError = true;
+    expdateField.classList.add("form-message-error-select");
+    inputExpDate.classList.add("form-control-error");
+  } else if (valueYY === undefined || valueYY === "YY") {
+    hasError = true;
+    yyfield.classList.add("form-message-error-select");
+    inputMMYY.classList.add("form-control-error");
+  } else {
+    let inputs = document.querySelectorAll("input");
+    let inputArray = Array.from(inputs);
+    inputArray.forEach((input) => {
+      if (input.classList.contains("form-control-error")) {
+        hasError = true;
+      }
+    });
+    if (hasError) {
+      const toastLiveExample = document.getElementById("liveToast");
+      const toast = new bootstrap.Toast(toastLiveExample);
+      toast.show();
+    } else {
+      form.classList.add("d-none");
+      formComplete.classList.remove("d-none");
+      $productContainer.classList.add("d-none")
     }
-    else if(valueYY === undefined || valueYY === "YY"){
-        hasError = true
-        yyfield.classList.add("form-message-error-select")
-        inputMMYY.classList.add("form-control-error")
-    }
-    else{
-        let inputs = document.querySelectorAll("input")
-        let inputArray = Array.from(inputs)
-        inputArray.forEach(input => {
-            if(input.classList.contains("form-control-error")){
-                hasError = true
-            }
-        })
-        if(hasError){
-            const toastLiveExample = document.getElementById('liveToast')
-            const toast = new bootstrap.Toast(toastLiveExample)
-            toast.show()
-        }
-        else{
-            form.classList.add("d-none")
-            formComplete.classList.remove("d-none")
-        }
-    }
-})
+  }
+});
 
+const clock = document.getElementById("timer");
+let tempoRestante = 31 * 60000;
+let intervalo;
+let ultimoTempo = Date.now();
 
+const atualizarTempo = () => {
+  let tempoAgora = Date.now();
+  let tempoDecorrido = tempoAgora - ultimoTempo;
+  ultimoTempo = tempoAgora;
+
+  tempoRestante -= tempoDecorrido;
+
+  let minutos = Math.floor(tempoRestante / 60000);
+  let segundos = Math.floor((tempoRestante % 60000) / 1000);
+  let milisegundos = tempoRestante % 1000;
+
+  minutos = minutos < 10 ? `0${minutos}` : minutos;
+  segundos = segundos < 10 ? `0${segundos}` : segundos;
+  milisegundos = milisegundos < 100 ? `0${milisegundos}` : milisegundos < 10 ? `0${milisegundos}` : milisegundos;
+
+  let timerFormatado = `${minutos}:${segundos}:${milisegundos}`;
+  clock.textContent = timerFormatado;
+  if (tempoRestante <= 0) {
+    clock.textContent = "O tempo acabou!";
+    clearInterval(intervalo);
+  }
+};
+
+intervalo = setInterval(atualizarTempo, 10);
+atualizarTempo();
